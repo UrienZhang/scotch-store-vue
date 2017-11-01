5f3819586e3ae289107379c983daaa6fd2fcfa37
Josh Naylor <joshnaylor88@gmail.com>
2017-10-31 21:34:51 -0500

    added express

--- src/components/products/ProductForm.vue
+++ src/components/products/ProductForm.vue
@@ -7,11 +7,11 @@
       </div>
       <div class="form-group">
         <label>Price</label>
-        <input type="number" class="form-control" placeholder="Price" v-model="model.price" name="price" class="form-control" />
+        <input type="number" class="form-control" placeholder="Price" v-model="model.price" name="price" />
       </div>
       <div class="form-group">
         <label>Manufacturer</label>
-        <select type="text" class="form-control" v-model="model.manufacturer" name="manufacturer" class="form-control">
+        <select type="text" class="form-control" v-model="model.manufacturer" name="manufacturer">
           <template v-for="manufacturer in manufacturers">
             <option :value="manufacturer._id" :selected="manufacturer._id == (model.manufacturer && model.manufacturer._id)">{{manufacturer.name}}</option>
           </template>
@@ -26,7 +26,7 @@
       </div>
       <div class="form-group">
         <label>Description</label>
-        <textarea class="form-control" placeholder="Description" rows="5" v-model="model.description" name="description" class="form-control"></textarea>
+        <textarea class="form-control" placeholder="Description" rows="5" v-model="model.description" name="description"></textarea>
       </div>
       <div class="form-group new-button">
         <button class="button">
@@ -43,10 +43,10 @@
 <script>
   export default {
     props: ['model', 'isEditing'],
-  },
-  methods: {
-    saveProduct () {
-      this.$emit('save-product', this.model)
-    }
-  }
+    methods: {
+      saveProduct() {
+        this.$emit('save-product', this.model);
+      },
+    },
+  };
 </script>
