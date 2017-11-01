bf01569265631ec8e69e94b55527f844f0c3a481
8ctopotamus <joshnaylor88@gmail.com>
2017-10-31 20:11:57 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <nav>
+      <div class="container">
+        <ul class="nav__left">
+          <li><router-link to="/"><i class="fa fa-home"></i> Home</router-link></li>
+          <li><router-link to="/admin"><i class="fa fa-user"></i> Admin</router-link></li>
+        </ul>
+        <ul class="nav__right">
+          <li><router-link to="/cart"><i class="fa fa-shopping-cart"></i> Cart </router-link></li>
+        </ul>
+      </div>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
